658c625b33848df2877a320d2844e9b6d82f54d8
anti_pode <ts@paraweb.me>
2022-11-26 20:23:23 +0700

    fix: popup types

--- src/api/items.types.ts
+++ src/api/items.types.ts
@@ -1,6 +1,7 @@
 export const enum PopupTypes {
   Promo,
   Story,
+  Personal,
 }
 
 export interface IPopup {
@@ -13,20 +14,41 @@ interface IPromoPopup extends IPopup {
   gift: string;
   code: string;
   link: string;
+  description?: string;
 }
 
-interface IStoryPopup extends IPopup {
-  type: PopupTypes.Story;
+interface IPersonalPopup extends IPopup {
+  type: PopupTypes.Personal;
   title: string;
   image: string;
   text: string;
+  author: string;
   supportLink: string;
-  aboutLink: string;
 }
+
+interface IStoryPopupBasic extends IPopup {
+  type: PopupTypes.Story;
+  title: string;
+  image?: string;
+  text: string;
+  hasSocialBlock: boolean;
+}
+
+interface IStoryPopupShare extends IStoryPopupBasic {
+  supportLink?: never;
+  shareUrl?: string;
+}
+
+interface IStoryPopupSupport extends IStoryPopupBasic {
+  supportLink: string;
+  shareUrl?: never;
+}
+
+type IStoryPopup = IStoryPopupShare | IStoryPopupSupport;
 
 export interface IItem {
   id: number;
   icon: string;
   isActive: boolean;
-  popup: IPromoPopup | IStoryPopup;
+  popup: IPromoPopup | IStoryPopup | IPersonalPopup;
 }
